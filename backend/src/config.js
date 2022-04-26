7154abdc642f673c3e3cecaaaf898a886b0ee362
DKGraphic <dkartofficial07@gmail.com>
2022-04-26 12:44:31 +0530

    updated config

--- backend/src/config.js
+++ backend/src/config.js
@@ -42,7 +42,6 @@ const extraMetadata = {
 };
 
 // NFTPort Info
-
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 2; // Your API key rate limit
