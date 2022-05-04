778b3b7c9df4916bfff5f86edb3a33513f71b450
DKGraphic <dkartofficial07@gmail.com>
2022-05-04 19:09:04 +0530

    updated generic

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const TOKENS_PER_MINT = 15; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-07-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-06-21T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x43Ae5E3642e874227A56216b15AC9a0c988FE159"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
@@ -70,7 +70,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xd2964e873E781dB70f0E11Ed00cD689b63723022"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
